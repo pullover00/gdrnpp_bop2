bd9f23ef27048b6c3a023b0b9fa6b8b17fa96257
Peter Hoenig <hpfreak1004@gmail.com>
2023-04-11 15:31:19 +0200

    upated docker related files

--- docker/docker_run.sh
+++ docker/docker_run.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
 
 # prepare /datasets, /pretrained_models and /output folders as explained in the main README.md
-gdrnpp_dir=${PWD%/*}
-
 docker run \
 --gpus all \
 -it \
 --shm-size=8gb --env="DISPLAY" \
---volume="${gdrnpp_dir}:/gdrnpp_bop2022" \
---volume="${gdrnpp_dir}/datasets:/gdrnpp_bop2022/datasets" \
---volume="${gdrnpp_dir}/pretrained_models:/gdrnpp_bop2022/pretrained_models" \
---volume="${gdrnpp_dir}/output:/gdrnpp_bop2022/output" \
+--volume="/home/hoenig/BOP/GDRNet:/gdrnpp" \
 --name=gdrnppv0 gdrnpp
